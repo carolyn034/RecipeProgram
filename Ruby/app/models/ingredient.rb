d7ee9c6f261935da36b6544c28bba22623522eaa
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-05-22 13:56:38 +1200

    some corrections to ingredient and recipe model syntax and rake file

--- Ruby/app/models/ingredient.rb
+++ Ruby/app/models/ingredient.rb
@@ -1,8 +1,8 @@
 require_relative '../../db/config'
 
 class Ingredient < ActiveRecord::Base
-  has_many :ingredient_recipe
-  has_many :recipe through :ingredient_recipe
+  has_many :ingredient_recipes
+  has_many :recipe, through: :ingredient_recipes
 
   def select_ingredients
     #select ingredients taged to recipe
