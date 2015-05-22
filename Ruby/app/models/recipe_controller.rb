023c133a56a676f2aa1c322f35734b9c3cf2d760
elisamckoy <apprentice@StMac8.local>
2015-05-22 12:27:48 +1200

    setup for models and controllers

---
+++ Ruby/app/models/recipe_controller.rb
@@ -0,0 +1,16 @@
+require_relative '../../db/config'
+
+class RecipeController < ActiveRecord::Base
+
+  def print_list_of_recipes
+    puts Recipe.all
+  end
+
+  def shoping_list
+    #printing of ingredients with qty
+  end
+
+  def help
+    # shows methods avalible for users
+  end
+end
